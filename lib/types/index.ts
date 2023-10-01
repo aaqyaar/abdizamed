87d325b67dcd7ae4a6b14110e12607bbb72a8461
abdizamedmo <abdizamedmo@gmail.com>
2023-10-01 23:15:26 +0300

    Posts export type removed

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,4 +1,3 @@
-export type { Post, Posts } from "./post.types";
 export type { Project, Projects } from "./project.types";
 export type { IAbout, IWorkExperience } from "./about.types";
 export type { TContact } from "./contact.types";
