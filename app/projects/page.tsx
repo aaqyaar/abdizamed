70772c8d04cf99efc13dca98ba2751997c13a0e2
abdizamedmo <abdizamedmo@gmail.com>
2023-10-01 22:33:50 +0300

    The projects page added sooyaal mobile app for somalia

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -46,9 +46,8 @@ const projects: Projects = [
     description:
       "A Private Apartment is an online booking platform with a sleek design powered by React.js and Material UI. The robust backend uses Node.js and Express.js for efficient user request handling. Data is stored in MongoDB, and a custom CMS allows easy property and content management. The website is hosted on Heroku and the database on MongoDB Atlas.",
   },
-
   {
-    id: 4,
+    id: 3,
     images: [
       "/simple-bank/1.png",
       "/simple-bank/1.png",
@@ -60,6 +59,16 @@ const projects: Projects = [
     technologies: ["React.js", "Material UI", "ASP.NET", "PostgreSQL"],
     description:
       "Simple Bank is a simple banking system that allows users to create accounts, deposit, withdraw, and transfer money. The frontend is built with React.js and Material UI. The backend is built with ASP.NET and PostgreSQL. Also included is a custom CMS for the admin to manage users and transactions and a REST API for the frontend to consume.",
+  },
+
+  {
+    id: 4,
+    images: ["/sooyaal/1.png", "/sooyaal/2.png", "/sooyaal/3.png"],
+    title: "Sooyaal (App)",
+    github: "https://www.github.com/aaqyaar/alhilaal-real-state",
+    technologies: ["React Native", "Expo", "Nest.js", "MongoDB"],
+    description:
+      "Sooyaal (App) is a social media app that allows somalian users to share their thoughts, ideas, and events. Also included is a chat feature that allows users to chat with each other. and also a somali people can able to see the somali weekly events. The frontend is built with React Native and Expo. The backend is built with Nest.js and MongoDB.",
   },
 ];
 
