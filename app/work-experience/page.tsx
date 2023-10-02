0ec1782b353ce38f023026804784806f00ed32a2
abdizamedmo <abdizamedmo@gmail.com>
2023-10-02 10:42:58 +0300

    work experience updated

--- app/work-experience/page.tsx
+++ app/work-experience/page.tsx
@@ -8,7 +8,7 @@ const workExperience: IWorkExperience[] = [
     startDate: "2022-01-01",
     endDate: "Present",
     description:
-      "I have been working as a freelance software developer for the past 2 years. I have worked with clients in somalia and also outside somalia. I have also worked on a variety of projects, including web applications, mobile apps, and desktop apps. I have also worked on a variety of technologies, including React, React Native, Node.js, Express.js, MongoDB, PostgreSQL, and Firebase. I have also worked on a variety of projects, including web applications, mobile apps, and desktop apps. I have also worked on a variety of technologies, including React, React Native, Node.js, Express.js, MongoDB, PostgreSQL, and Firebase.",
+      "In the realm of freelance development, I have consistently demonstrated an unwavering commitment to bringing both local and remote clients' visions into reality. Drawing from a refined technological repertoire, I've specialized in harnessing the power of <strong>ASP.NET</strong> and <strong>Node.js</strong> for backend development, ensuring robust, secure, and scalable server-side solutions. On the frontend, my proficiency with <strong>React.js</strong> and <strong>Next.js</strong> has been instrumental in devising dynamic, responsive, and intuitively interactive user interfaces. Furthermore, my adeptness with <strong>PostgreSQL</strong> guarantees efficient, structured, and reliable data management. This comprehensive tech stack not only underscores my versatility but also my dedication to delivering products that resonate with the envisioned idea.",
   },
   {
     company: "Silicon Solutions",
@@ -25,7 +25,7 @@ const workExperience: IWorkExperience[] = [
     endDate: "Present",
 
     description:
-      "In my current role as a Mobile App Developer at Bixi, I am entrusted with the responsibility of conceptualizing, developing, and implementing new features for mobile applications. My primary focus lies in creating user-friendly and innovative apps, which entails leveraging cutting-edge technologies like React Native to build dynamic and responsive interfaces. Additionally, I am deeply committed to optimizing app performance to ensure seamless user experiences. As part of my role, I actively participate in various stages of the app development process, from initial ideation and design to rigorous testing and final deployment. I also collaborate with cross-functional teams to define, design, and ship new features. I am also responsible for continuously discovering, evaluating, and implementing new technologies to maximize development efficiency.",
+      "In my current role as a Mobile App Developer at Bixi, I am entrusted with the responsibility of conceptualizing, developing, and implementing new features for mobile applications. My primary focus lies in creating user-friendly and innovative apps, which entails leveraging cutting-edge technologies like <strong>React Native</strong> to build dynamic and responsive interfaces. Additionally, I am deeply committed to optimizing app performance to ensure seamless user experiences. As part of my role, I actively participate in various stages of the app development process, from initial ideation and design to rigorous testing and final deployment. I also collaborate with cross-functional teams to define, design, and ship new features. I am also responsible for continuously discovering, evaluating, and implementing new technologies to maximize development efficiency.",
   },
 ];
 
@@ -52,7 +52,12 @@ export default function WorkExperiencePage() {
                 </h2>
               </div>
               <h3 className="text-xl font-bold">{work.position}</h3>
-              <p className="font-light">{work.description}</p>
+              <p
+                className="font-light"
+                dangerouslySetInnerHTML={{
+                  __html: work.description,
+                }}
+              />
             </div>
           ))}
         </div>
