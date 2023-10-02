e2060d869121a88c53012f1852ea01f2be5e0688
abdizamedmo <abdizamedmo@gmail.com>
2023-10-02 10:42:11 +0300

    Header spacing added - gap for the items

--- components/header.tsx
+++ components/header.tsx
@@ -6,18 +6,18 @@ import { Button } from "./ui/button";
 
 export default function Header() {
   return (
-    <header className="py-2 max-w-4xl mx-4 lg:mx-auto flex justify-between items-center">
+    <header className="py-2 max-w-5xl mx-4 gap-4 lg:mx-auto flex justify-between items-center">
       <div className="relative w-14 h-14">
         <Link href={"/"}>
           <Image
-            src={"https://www.github.com/aaqyaar.png"}
+            src={"/icon.png"}
             fill
             alt="Abdi Zamed Mohamed"
-            className="rounded-full"
+            className="border-4 rounded-full"
           />
         </Link>
       </div>
-      <nav className="flex items-center gap-4">
+      <nav className="flex items-center gap-2">
         <Link href={"/work-experience"}>
           <Button variant={"ghost"}>Work Experience</Button>
         </Link>
